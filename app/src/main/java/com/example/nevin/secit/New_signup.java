271240f493f64f34e52da79da4bf40921ddb24be
Nevin20 <nevinrocky6@gmail.com>
2019-04-04 16:12:49 +0530

    signup updated

--- app/src/main/java/com/example/nevin/secit/New_signup.java
+++ app/src/main/java/com/example/nevin/secit/New_signup.java
@@ -2,12 +2,40 @@ package com.example.nevin.secit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class New_signup extends AppCompatActivity {
+
+    EditText mobnum,mobotp;
+    CircleImageView verification;
+//    private FirebaseAuth mAuth;
+//    private String mVerificationId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_signup);
+
+        mobnum=findViewById(R.id.mobile_no);
+        mobotp=findViewById(R.id.otp);
+        verification=findViewById(R.id.cont_);
+
+        //mAuth= FirebaseAuth.getInstance();
+//
+//        String mobile = mobnum.getText().toString().trim();
+//        if(mobile.isEmpty() || mobile.length() < 10){
+//            mobnum.setError("Enter a valid mobile");
+//            mobnum.requestFocus();
+//            return;
+//        }
+
+
+
+
     }
 }
